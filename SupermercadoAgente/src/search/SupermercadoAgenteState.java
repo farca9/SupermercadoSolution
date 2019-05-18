47f846179b2a776770f9c30196327bb0521ef16e
Facundo Arca <farca9@gmail.com>
2019-05-18 11:40:14 -0300

    Actualizacion mapa + Funcionando la compra

--- SupermercadoAgente/src/search/SupermercadoAgenteState.java
+++ SupermercadoAgente/src/search/SupermercadoAgenteState.java
@@ -86,62 +86,50 @@ public class SupermercadoAgenteState extends SearchBasedAgentState {
     	//Estado inicial del Agente
     	
     	//Inicializacion del mapa
-    	Mapa = new MapUnit[4][3];
+    	Mapa = new MapUnit[4][4];
     	
     	Mapa[0][0]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, true, false, false, false);
     	Mapa[0][1]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, true, false, false, false);
     	Mapa[0][2]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, false, false, false, true);
+    	Mapa[0][3]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
     	
     	Mapa[1][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
     	Mapa[1][1]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
     	Mapa[1][2]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, false, false, true, true);
+    	Mapa[1][3]= new MapUnit(TipoEnum.SUPERMERCADO, 0, 0, false, false, false, false);
     	
     	Mapa[2][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
     	Mapa[2][1]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, true, false, false, true);
     	Mapa[2][2]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, false, true, true, false);
+    	Mapa[2][3]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
     	
     	Mapa[3][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
     	Mapa[3][1]= new MapUnit(TipoEnum.SUPERMERCADO, 0, 0, false, false, false, false);
     	Mapa[3][2]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);    	    			
-    	/*
-    	Mapa = new MapUnit[5][3];
-    	
-    	Mapa[0][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	Mapa[0][1]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, false, false, false, true);
-    	Mapa[0][2]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	
-    	Mapa[1][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	Mapa[1][1]= new MapUnit(TipoEnum.CALLENORMAL, 0, 0, false, false, false, true);
-    	Mapa[1][2]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	
-    	Mapa[2][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	Mapa[2][1]= new MapUnit(TipoEnum.CALLENORMAL, 1, 1, false, false, false, true);
-    	Mapa[2][2]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	
-    	Mapa[3][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	Mapa[3][1]= new MapUnit(TipoEnum.CALLENORMAL, 0, 0, false, false, false, true);
-    	Mapa[3][2]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false); 
-    	
-    	Mapa[4][0]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	Mapa[4][1]= new MapUnit(TipoEnum.SUPERMERCADO, 0, 0, false, false, false, false);
-    	Mapa[4][2]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
-    	*/
+    	Mapa[3][3]= new MapUnit(TipoEnum.MANZANA, 0, 0, false, false, false, false);
+
     	
     	//Ubicacion Inicial
     	this.Ubicacion=new Point(0,0);
     	this.UbicacionAnterior=new Point(0,0);
     	
     	//Producto(s)
-    	Producto p = new Producto(1,"Huevos");
+    	Producto p1 = new Producto(1,"Huevos");
+    	Producto p2 = new Producto(2,"Pan");
+    	
+    	//Comercio(s)
+    	Comercio alvear = new Comercio(1,"Alvear", new Point(3,1));
+    	Comercio kilbel = new Comercio(2,"Kilbel", new Point(1,3));
     	
     	//Lista productos
     	this.ListaProductos=new HashMap();
-    	ListaProductos.put(p, false);
+    	ListaProductos.put(p1, false);
+    	ListaProductos.put(p2, false);
     	
     	//Matriz producto comercio
     	this.MatrizProductoComercio=new ArrayList();
-    	MatrizProductoComercio.add(new ProductoComercio(p, new Comercio(1,"Alvear", new Point(3,1)), 5.0));
-    	
+    	MatrizProductoComercio.add(new ProductoComercio(p1, alvear, 5.0));
+    	MatrizProductoComercio.add(new ProductoComercio(p2, kilbel, 3.0));
     	
     }
 
@@ -186,7 +174,7 @@ public class SupermercadoAgenteState extends SearchBasedAgentState {
         	Set<Entry<Producto, Boolean>> thisSet = this.ListaProductos.entrySet();
         	if(ListaProductos.size()!=state.ListaProductos.size()) {
         		return false;
-        	} else if (ListaProductos.equals(state.ListaProductos)) {
+        	} else if (!ListaProductos.equals(state.ListaProductos)) {
         		return false; 
         	}
         	
